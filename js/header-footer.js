907a16b0b2822affa6c02714b497e9fd8f2698cb
MathurSuhani <mathur.suhani206@gmail.com>
2025-06-13 14:55:05 +0530

    Intern Portal

---
+++ js/header-footer.js
@@ -0,0 +1,43 @@
+function initHeaderMenu() {
+  const menuToggle = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  if (!menuToggle || !mobileMenu) return;
+  menuToggle.addEventListener("click", () => {
+    if (mobileMenu.classList.contains("hidden")) {
+      mobileMenu.classList.remove("hidden");
+      setTimeout(() => mobileMenu.classList.remove(scale - y - 0), 10);
+      setTimeout(() => {
+        mobileMenu.classList.add("scale-y-100", 300);
+      });
+    } else {
+      mobileMenu.classList.add("scale-y-0");
+      mobileMenu.classList.remove("scale-y-100");
+      setTimeout(() => mobileMenu.classList.add("hidden"), 300);
+    }
+  });
+  document.querySelectorAll("#mobile-menu a").forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileMenu.classList.add("scale-y-0");
+      mobileMenu.classList.remove("scale-y-100");
+      setTimeout(() => {
+        mobileMenu.classList.add("hidden", 300);
+      });
+    });
+  });
+
+  const currentPage = window.location.pathname.split("/").pop().toLowerCase() || "index.html";
+  document.querySelectorAll(".nav-link").forEach((link) => {
+      const href = link.getAttribute("href").toLowerCase();
+      const underline = link.querySelector("span");
+      if(underline){
+        underline.classList.remove("w-full");
+        underline.classList.add("w-0");
+      }
+      if(href === currentPage){
+        underline.classList.remove("w-0");
+        underline.classList.add("w-full");
+      }
+    
+  });
+}
