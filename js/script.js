f8bdc53717d251b3c90d8437a116a17963c80c13
MathurSuhani <mathur.suhani206@gmail.com>
2025-06-15 14:30:13 +0530

    intern portal

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 AOS.init();
 
-  const swiper = new Swiper(".mySwiper", {
-  effect: 'coverflow',
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: 1,
   loop: true,
   autoplay: {
-    delay: 1500,   
-    disableOnInteraction: false
+    delay: 1500,
+    disableOnInteraction: false,
   },
   coverflowEffect: {
     rotate: 30,
@@ -18,7 +18,7 @@ AOS.init();
     slideShadows: true,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
@@ -26,3 +26,17 @@ AOS.init();
     prevEl: ".swiper-button-prev",
   },
 });
+function initBackToTop() {
+  const backToTop = document.getElementById("backToTopBtn");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      backToTop.classList.remove("opacity-0", "invisible");
+    } else {
+      backToTop.classList.add("opacity-0", "invisible");
+    }
+  });
+
+  backToTop.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+}
