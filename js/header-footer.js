9cde383a8d7da061ff7e69f6f7c49f63f1f38740
MathurSuhani <mathur.suhani206@gmail.com>
2025-06-16 09:15:21 +0530

    intern portal

--- js/header-footer.js
+++ js/header-footer.js
@@ -8,10 +8,10 @@ function initHeaderMenu() {
   menuToggle.addEventListener("click", () => {
     if (mobileMenu.classList.contains("hidden")) {
       mobileMenu.classList.remove("hidden");
-      setTimeout(() => mobileMenu.classList.remove(scale - y - 0), 10);
+      setTimeout(() => mobileMenu.classList.remove("scale-y-0"), 10);
       setTimeout(() => {
-        mobileMenu.classList.add("scale-y-100", 300);
-      });
+        mobileMenu.classList.add("scale-y-100");
+      }, 10);
     } else {
       mobileMenu.classList.add("scale-y-0");
       mobileMenu.classList.remove("scale-y-100");
