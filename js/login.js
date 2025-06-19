c0c6b9d53bf0d0eeafd2a10f3a76657824dc7a62
MathurSuhani <mathur.suhani206@gmail.com>
2025-06-19 18:46:12 +0530

    intern portal

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.getElementById("loginForm");
   const registerForm = document.getElementById("registerForm");
 
-  const endpoint = "https://script.google.com/macros/s/AKfycbxNTGe4rpfpArTcj0GtuzdBuJ5ldPcXC_JswKQUoKzxNFawRVJEElIzMONXDhY0BW-r0Q/exec"; // <-- Replace this with your deployed Web App URL
+  
 
   showRegister.addEventListener("click", (e) => {
     e.preventDefault();
